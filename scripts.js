ccedbb73fd9daedf3b9ff8e18b6207f3ebd760b5
TheodorosPloumis <me@theodorosploumis.com>
2017-02-01 20:54:25 +0200

    Initial

---
+++ scripts.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+
+  var main = $("#main-texts"),
+      mainId = $("#main-id"),
+      quotes = $("#quotes p"),
+      count = quotes.length,
+      countdown = $("#countdown"),
+      pause = $("#pause"),
+      random = $("#random"),
+      body = $("body"),
+      cnt = 0,
+      tencnt = 0,
+      go = true;
+
+  startTimer();
+
+  // Random quote
+  random.click(function(){
+    stopTimer();
+    tencnt = 0;
+    everyTen();
+  });
+
+  // Pause slider
+  pause.click(function(){
+    var el = $(this);
+
+    if (go) {
+      stopTimer();
+    } else {
+      startTimer();
+    }
+  });
+
+  // Get quote by hash
+  if(window.location.hash) {
+    var id = window.location.hash.substr(1);
+
+    if (id > count) {
+      id = 0;
+    }
+
+    stopTimer();
+    main.text(quotes.eq(id).text());
+    mainId.text(id);
+  }
+
+  // Helper functions
+  function everyTen(){
+    var random = Math.floor(Math.random() * count) + 1;
+
+    tencnt++;
+
+    $("#countdown").text(tencnt);
+    main.text(quotes.eq(random).text());
+    mainId.text(random);
+  }
+
+  function stopTimer(){
+    go = false;
+    pause.text("Start");
+    body.addClass("js-active");
+  }
+
+  function startTimer(){
+    go = true;
+    pause.text("Stop");
+    quoteTimer();
+    body.removeClass("js-active");
+  }
+
+  // SetInterval loop
+  function quoteTimer() {
+    if(!go) {
+      return;
+    }
+
+    cnt++;
+
+    if(cnt >= 10) {
+      cnt = 0;
+      everyTen();
+    }
+    $("#countdown").text(cnt);
+    setTimeout(quoteTimer, 1000);
+  }
+
+});
